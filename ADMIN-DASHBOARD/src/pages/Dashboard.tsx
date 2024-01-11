b509134707ae61842ba32218928bd17bc804f59e
GAURAV <gaurav1993rajput@gmail.com>
2024-01-11 10:42:14 +0530

    Revenue and trasanction chart added

--- ADMIN-DASHBOARD/src/pages/Dashboard.tsx
+++ ADMIN-DASHBOARD/src/pages/Dashboard.tsx
@@ -4,6 +4,7 @@ import { BsSearch } from "react-icons/bs";
 import { HiTrendingUp, HiTrendingDown } from "react-icons/hi"
 import userPic from "../assets/userPic.png";
 import data from "../assets/data.json";
+import { BarChart } from "../components/Charts";
 
 const Dashboard = () => {
   return (
@@ -47,7 +48,14 @@ const Dashboard = () => {
         <section className="graph-container">
           <div className="revenue-chart">
             <h2>Revenue & Transaction</h2>
-            {/* Grapph here */}
+            <BarChart
+              data_2={[300, 144, 433, 655, 237, 755, 190]}
+              data_1={[200, 444, 343, 556, 778, 455, 990]}
+              title_1="Revenue"
+              title_2="Transaction"
+              bgColor_1="rgb(0,115,255)"
+              bgColor_2="rgba(53,162,235,0.8)"
+            />
 
           </div>
 
@@ -140,3 +148,5 @@ const CategoryItem = ({ color, value, heading }: CategoryItemProps) => (
 );
 
 export default Dashboard
+
+
