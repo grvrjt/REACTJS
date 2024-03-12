1b02add3a65ec9294992cec5cdb7ad7ddbbd44b4
GAURAV <gaurav1993rajput@gmail.com>
2024-03-12 12:12:35 +0530

    refecth onClick

--- react-query/src/components/RQSuperHeroes.page.js
+++ react-query/src/components/RQSuperHeroes.page.js
@@ -6,7 +6,7 @@ const fetchSuerHeroes = () => {
 }
 
 export const RQSuperHeroesPage = () => {
-  let { isLoading, data, isError, error, isFetching } = useQuery(
+  let { isLoading, data, isError, error, isFetching, refetch } = useQuery(
     'super-heroes',
     fetchSuerHeroes,
     {
@@ -14,18 +14,22 @@ export const RQSuperHeroesPage = () => {
       // staleTime: 30000
       // refetchOnMount: false true, 'always'
       // refetchOnWindowFocus: true
-      refetchInterval: 2000,
-      refetchIntervalInBackground: true
+      // refetchInterval: 2000,
+      // refetchIntervalInBackground: true,
+      
+      enabled: false
+
     }
   );
   console.log({ isLoading, isFetching, data })
-  if (isLoading) {
+  if (isLoading || isFetching) {
     return <h2>Loading....</h2>
   }
   if (isError) {
     return <h2>{error.message}</h2>
   }
   return <><h2>React Query Super Heroes Page</h2>
+  <button onClick={refetch}>Fecth the heroes</button>
     {data?.data.map((hero) => {
       return <div key={hero.name}>{hero.name}</div>
     })}
