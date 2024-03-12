e498012f3fec291166bdc825759aeb990078a3c5
GAURAV <gaurav1993rajput@gmail.com>
2024-03-12 12:25:44 +0530

    onSuccess and onError

--- react-query/src/components/RQSuperHeroes.page.js
+++ react-query/src/components/RQSuperHeroes.page.js
@@ -5,6 +5,12 @@ const fetchSuerHeroes = () => {
   return axios.get('http://localhost:4000/superheroes');
 }
 
+const onSuccess = (data) => {
+  console.log("Side effect after on sucess !", data)
+}
+const onError = (error) => {
+  console.log("Side effect after on error !", error.message)
+}
 export const RQSuperHeroesPage = () => {
   let { isLoading, data, isError, error, isFetching, refetch } = useQuery(
     'super-heroes',
@@ -16,8 +22,11 @@ export const RQSuperHeroesPage = () => {
       // refetchOnWindowFocus: true
       // refetchInterval: 2000,
       // refetchIntervalInBackground: true,
-      
-      enabled: false
+
+      // enabled: false
+
+      onSuccess: onSuccess,
+      onError: onError
 
     }
   );
@@ -29,7 +38,7 @@ export const RQSuperHeroesPage = () => {
     return <h2>{error.message}</h2>
   }
   return <><h2>React Query Super Heroes Page</h2>
-  <button onClick={refetch}>Fecth the heroes</button>
+    {/* <button onClick={refetch}>Fecth the heroes</button> */}
     {data?.data.map((hero) => {
       return <div key={hero.name}>{hero.name}</div>
     })}
