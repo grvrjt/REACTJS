5e73dd405832eafc06aa988dddc9ce6dd4ffe9eb
GAURAV <gaurav1993rajput@gmail.com>
2024-03-12 10:40:00 +0530

    React query added

---
+++ react-query/src/components/RQSuperHeroes.page.js
@@ -0,0 +1,20 @@
+import { useQuery } from "react-query";
+import axios from "axios";
+
+const fetchSuerHeroes = () => {
+  return axios.get('http://localhost:4000/superheroes');
+}
+
+export const RQSuperHeroesPage = () => {
+  const { isLoading, data } = useQuery('super-heroes', fetchSuerHeroes);
+console.log( { isLoading, data } )
+  if (isLoading) {
+    return <h2>Loading....</h2>
+  }
+
+  return <><h2>React Query Super Heroes Page</h2>
+    {data?.data.map((hero) => {
+      return <div key={hero.name}>{hero.name}</div>
+    })}
+  </>
+}
